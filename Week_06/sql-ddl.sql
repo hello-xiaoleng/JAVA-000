91c18eb59e7f904bfc3f20385ec43b30c466c20c
chenjia <2496243148@99.com>
2020-11-25 18:53:33 +0800

    第六周作业

---
+++ Week_06/sql-ddl.sql
@@ -0,0 +1,31 @@
+CREATE TABLE `user` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(50) COLLATE utf8mb4_bin NOT NULL COMMENT '昵称',
+  `mobile` varchar(11) NOT NULL COMMENT '手机号',
+  `create_time` int NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `update_time` int NOT NULL DEFAULT '0' COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='用户表';
+
+CREATE TABLE `goods` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_bin NOT NULL COMMENT '商品名称',
+  `price` int NOT NULL DEFAULT '0' COMMENT '商品价格：分',
+  `create_time` int NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `update_time` int NOT NULL DEFAULT '0' COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='商品表';
+
+CREATE TABLE `order` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `order_no` varchar(40) NOT NULL COMMENT '订单号',
+  `user_id` int NOT NULL COMMENT '用户id',
+  `good_id` int NOT NULL COMMENT '商品id',
+  `good_price` int NOT NULL COMMENT '商品价格：分',
+  `count` int NOT NULL COMMENT '商品数量',
+  `amount` int NOT NULL DEFAULT '0' COMMENT '订单金额：分',
+  `order_status` int NOT NULL DEFAULT '0' COMMENT '订单状态',
+  `create_time` int NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `update_time` int NOT NULL DEFAULT '0' COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='订单表';
